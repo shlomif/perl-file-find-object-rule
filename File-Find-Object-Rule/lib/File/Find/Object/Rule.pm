43184c5c362c8bfb26fdf923af30a4df623c18cb
Shlomi Fish <shlomif@shlomifish.org>
2013-05-27 17:53:11 +0300

    Bump ver to 0.0303

--- File-Find-Object-Rule/lib/File/Find/Object/Rule.pm
+++ File-Find-Object-Rule/lib/File/Find/Object/Rule.pm
@@ -14,7 +14,7 @@ use File::Find::Object; # we're only wrapping for now
 use File::Basename;
 use Cwd;           # 5.00503s File::Find goes screwy with max_depth == 0
 
-$VERSION = '0.0302';
+$VERSION = '0.0303';
 
 use Class::XSAccessor
     accessors => {
