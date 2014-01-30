9cf7527cefc18a603d46dda1ed178375d4c9ac5d
Shlomi Fish <shlomif@shlomifish.org>
2014-01-30 16:05:13 +0200

    New version 0.0.3

--- File-Find-Object-Rule-MMagic/lib/File/Find/Object/Rule/MMagic.pm
+++ File-Find-Object-Rule-MMagic/lib/File/Find/Object/Rule/MMagic.pm
@@ -10,7 +10,7 @@ use File::Find::Object::Rule;
 use base qw( File::Find::Object::Rule );
 use vars qw( $VERSION @EXPORT );
 @EXPORT  = @File::Find::Object::Rule::EXPORT;
-$VERSION = '0.0.2';
+$VERSION = '0.0.3';
 
 use File::MMagic;
 use Text::Glob qw(glob_to_regex);
