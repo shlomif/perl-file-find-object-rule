44b23bd9b4d7d673af7877201add5c886d9b2551
Shlomi Fish <shlomif@shlomifish.org>
2014-02-02 22:25:17 +0200

    Change use base to use parent

--- File-Find-Object-Rule-MMagic/lib/File/Find/Object/Rule/MMagic.pm
+++ File-Find-Object-Rule-MMagic/lib/File/Find/Object/Rule/MMagic.pm
@@ -7,10 +7,10 @@ use warnings;
 use 5.008;
 
 use File::Find::Object::Rule;
-use base qw( File::Find::Object::Rule );
+use parent qw( File::Find::Object::Rule );
 use vars qw( $VERSION @EXPORT );
 @EXPORT  = @File::Find::Object::Rule::EXPORT;
-$VERSION = '0.0.3';
+$VERSION = '0.0.4';
 
 use File::MMagic;
 use Text::Glob qw(glob_to_regex);
